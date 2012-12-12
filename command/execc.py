e764a5c511bafb053e494e7ad35f608a9c8e5dcc
gejza <gejza@rorschach>
2012-12-12 12:29:25 +0100

    --failed for exec

--- command/execc.py
+++ command/execc.py
@@ -12,7 +12,9 @@ class Exec(object):
     @staticmethod
     def add_arguments(subparser):
         subparser.add_argument("statement",help="python statement")
-        subparser.add_argument("-q","--quiet",action="store_true",help="supress output stream")
+        group=subparser.add_mutually_exclusive_group()
+        group.add_argument("-q","--quiet",action="store_true",help="supress output stream")
+        group.add_argument("-f","--failed",action="store_true",help="output only the failed records,supress error message")
 
     def __init__(self,ns):
         self.ns=ns
@@ -22,11 +24,17 @@ class Exec(object):
             try:
                 exec self.ns.statement in d
             except:
-                print >> sys.stderr, "exec: Warning: failed on item %s" % describe_item(i,d)
-#                print >> sys.stderr, sys.exc_info[0]
+                if self.ns.failed:
+                    if '__builtins__' in d:    
+                        del d['__builtins__']
+                    print "---"
+                    sys.stdout.write(yaml_dump_encoded(d))
+                else:
+                    print >> sys.stderr, "exec: Warning: failed on item %s" % describe_item(i,d)
+                    print >> sys.stderr, "exec: The exception was %s" % sys.exc_info()[0]
             if '__builtins__' in d:    
                 del d['__builtins__']
-            if not self.ns.quiet:
+            if not self.ns.quiet and not self.ns.failed:
                 print "---"
                 sys.stdout.write(yaml_dump_encoded(d))
 
