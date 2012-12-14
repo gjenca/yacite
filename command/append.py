774ad8c932336b0ff40a7585b561ad651136dd1e
gejza <gejza@rorschach>
2012-12-14 17:24:14 +0100

    terminology -append

--- command/append.py
+++ command/append.py
@@ -2,35 +2,36 @@
 
 import sys
 from yacite.utils.sane_yaml import record_stream,yaml_dump_encoded
+from yacite.utils.misc import describe_item
 from yacite.exception import *
 
 class Append(object):
 
 
-    help="appends each string in the list to the value of a variable"
+    help="appends each string in the list to the value of a field"
 
     @staticmethod
     def add_arguments(subparser):
-        subparser.add_argument("variable",help="variable. Value of variable must be 'list of strings'.")
+        subparser.add_argument("fieldname",help="Field name. Value must be a 'list of strings'.")
         subparser.add_argument("string",nargs="+",help="these strings are appended to the value")
 
     def __init__(self,ns):
         self.ns=ns
 
     def execute(self):
-        for i,d in enumerate(record_stream(sys.stdin)):
-            if self.ns.variable in d:
-                value=d[self.ns.variable]
+        for i,rec in enumerate(record_stream(sys.stdin)):
+            if self.ns.fieldname in rec:
+                value=rec[self.ns.fieldname]
                 if type(value) is not list:
-                    raise DataError("append: expecting list under variable %s in item %d, got %s instead" %
-                        (self.ns.variable,i,type(value)))
+                    raise DataError("append: expecting a list under %s in %s, got %s instead" %
+                        (self.ns.fieldname,describe_item(i,rec),type(value)))
                 value.extend(self.ns.string)
                 value=list(set(value))
-                d[self.ns.variable]=value
+                rec[self.ns.fieldname]=value
             else:
-                d[self.ns.variable]=[s.decode('utf-8') for s in self.ns.string]
+                rec[self.ns.fieldname]=[s.decode('utf-8') for s in self.ns.string]
             print "---"
-            sys.stdout.write(yaml_dump_encoded(d))
+            sys.stdout.write(yaml_dump_encoded(rec))
 
                 
         
