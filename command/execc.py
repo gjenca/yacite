6b03e9e7553fbe6f4f7c448cda08e1b6ccab8bbe
gejza <gejza@rorschach>
2012-12-14 16:58:17 +0100

    the logic of filter and exec altered

--- command/execc.py
+++ command/execc.py
@@ -14,12 +14,14 @@ class Exec(object):
         subparser.add_argument("statement",help="python statement")
         group=subparser.add_mutually_exclusive_group()
         group.add_argument("-q","--quiet",action="store_true",help="supress output stream")
+        subparser.add_argument("-k","--keep-going",action="store_true",help="do not stop when the statement throws an exception")
         group.add_argument("-f","--failed",action="store_true",help="output only the failed records,supress error message")
 
     def __init__(self,ns):
         self.ns=ns
 
     def execute(self):
+        exceptions=0
         for i,d in enumerate(docstream(sys.stdin)):
             try:
                 exec self.ns.statement in d
@@ -29,14 +31,20 @@ class Exec(object):
                         del d['__builtins__']
                     print "---"
                     sys.stdout.write(yaml_dump_encoded(d))
-                else:
+                elif self.ns.keep_going:
+                    exceptions+=1
                     print >> sys.stderr, "exec: Warning: failed on item %s" % describe_item(i,d)
                     print >> sys.stderr, "exec: The exception was %s" % sys.exc_info()[0]
+                else:
+                    raise
             if '__builtins__' in d:    
                 del d['__builtins__']
             if not self.ns.quiet and not self.ns.failed:
                 print "---"
                 sys.stdout.write(yaml_dump_encoded(d))
+        if exceptions and not self.ns.failed:
+            print >> sys.stderr, "exec: Warning: there were %d exceptions" % exceptions
+            
 
                 
         
--- command/filter.py
+++ command/filter.py
@@ -19,11 +19,13 @@ class Filter(object):
             help="filter applies only is myown == False or undefined, otherwise the record passes through")
         subparser.add_argument("expr",help="python expression")
         subparser.add_argument("-f","--failed",action="store_true",help="output only the failed records,supress error message")
+        subparser.add_argument("-k","--keep-going",action="store_true",help="do not stop when the eval(expr) throws an exception")
 
     def __init__(self,ns):
         self.ns=ns
 
     def execute(self):
+        exceptions=0
         for i,d in enumerate(docstream(sys.stdin)):
             if self.ns.myown:
                 if (not u"myown" in d) or d[u"myown"]==False:
@@ -43,14 +45,19 @@ class Filter(object):
                         del d['__builtins__']
                     print "---"
                     sys.stdout.write(yaml_dump_encoded(d))
+                elif self.ns.keep_going:
+                    exceptions+=1
+                    print >> sys.stderr, "filter: Warning: failed on item %s" % describe_item(i,d)
+                    print >> sys.stderr, "filter: The exception was %s" % sys.exc_info()[0]
                 else:
-                    print >> sys.stderr, "exec: Warning: failed on item %s" % describe_item(i,d)
-                    print >> sys.stderr, "exec: The exception was %s" % sys.exc_info()[0]
+                    raise
             else:
                 if not self.ns.failed and tf:
                     print "---"
                     sys.stdout.write(yaml_dump_encoded(d))
 
-                
+        if exceptions and not self.ns.failed:
+            print >> sys.stderr, "exec: Warning: there were %d exceptions" % exceptions
+            
         
         
